7e13e3034fbb4b8bd991a6da34d0e61fdc4c36c8
Anton Anatolievich Gridushko <anton.gridushko@gmail.com>
2019-09-19 22:59:35 +0300

    [+]

--- NET.A.2019.Gridushko.19/Program.cs
+++ NET.A.2019.Gridushko.19/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Xml.Linq;
+using System.Xml.Schema;
 
-namespace NET.W._2019.Gridushko._19
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace NET.A._2019.Gridushko._19
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            XmlSchemaSet schema = new XmlSchemaSet();
+            schema.Add("", @"C:\Users\User\source\repos\NET.A.2019\NET.A.2019.Gridushko.19\URL.xsd");
+
+            XDocument doc = XDocument.Load(@"C:\Users\User\source\repos\NET.A.2019\NET.A.2019.Gridushko.19\Data.xml");
+
+            bool validationErrors = false;
+
+            doc.Validate(schema, (s, e) =>
+                {
+                    Console.WriteLine(e.Message);
+                    validationErrors = true;
+                });
+
+            if (validationErrors)
+            {
+                log.Info("Validation failed!");
+                Console.WriteLine("Validation failed");
+            }
+            else
+            {
+                Console.WriteLine("Validation Succeeded");
+            }
         }
     }
 }
